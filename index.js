cd10f7c2bff341ab58349e2b7ce96f3aa42d5603
diegobrice <diegobrice@hotmail.com>
2020-07-06 00:48:47 -0500

    crud working

---
+++ index.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCxBo81Zu5fNGNu9BhO8L4zhoOwLN_kMG4",
+  authDomain: "todoapp-e41bc.firebaseapp.com",
+  databaseURL: "https://todoapp-e41bc.firebaseio.com",
+  projectId: "todoapp-e41bc",
+  storageBucket: "todoapp-e41bc.appspot.com",
+  messagingSenderId: "828672421286",
+  appId: "1:828672421286:web:8c056e4500b36f83a7f367",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
